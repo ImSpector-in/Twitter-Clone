a77a469e05a9e9e636eababfa41477db6e7440c4
Alex <michael@ykona.com>
2026-05-07 14:44:38 -1000

    Fix password eye icon to show current state not action

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -22,7 +22,7 @@ export default function PasswordInput({ className, ...props }: Props) {
         className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-foreground transition-colors"
         tabIndex={-1}
       >
-        {show ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
+        {show ? <Eye className="h-4 w-4" /> : <EyeOff className="h-4 w-4" />}
       </button>
     </div>
   )
